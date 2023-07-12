0d419d146e479c7e50dda774e4f49742f5b094fe
Tormod <tormod.flesjo@gmail.com>
2023-07-12 02:00:26 +0200

    quick commit

--- utils/routes.js
+++ utils/routes.js
@@ -6,7 +6,7 @@ function wakeUpServer() {
   axios
     .get(`${baseurl}/wakeup`)
     .then((response) => {
-      if (response.status === 200) {
+      if ([204, 201, 200].includes(response.status)) {
         game.menu.serversOnline = true;
         game.menu.highScore = response.data.highScore;
         game.menu.preloadImages();
@@ -20,9 +20,9 @@ function wakeUpServer() {
 // create player instance and save id in clientside
 function startGame() {
   axios
-    .post(`${baseurl}/startGame`) // todo, change out URL with heroku server
+    .post(`${baseurl}/startGame`)
     .then((response) => {
-      if (response.status === 200) {
+      if ([204, 201, 200].includes(response.status)) {
         game.gameStarted = true;
         game.music.play();
         game.playerName = response.data.name;
@@ -37,7 +37,7 @@ function levelComplete(data) {
   axios
     .put(`${baseurl}/updateStats`, data)
     .then((response) => {
-      if (response.status === 200) {
+      if ([204, 201, 200].includes(response.status)) {
         /* if (response.data.cheater === true) {
                     return goBackToHomePage()
                 } */
@@ -53,12 +53,14 @@ function finishGame(playerName, newName) {
   axios
     .put(`${baseurl}/endGame`, { playerName, newName })
     .then((response) => {
-      if (response.status === 200) {
+      if ([204, 201, 200].includes(response.status)) {
         // todo, display final score for user when submit
         game.hud.finalScore = response.data.score || 0;
       }
     })
-    .catch((error) => {});
+    .catch((error) => {
+      console.error(error);
+    });
   goBackToHomePage();
 }
 
